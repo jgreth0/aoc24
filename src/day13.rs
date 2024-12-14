4da03967bc3923f00eefbd0da58cd9626ac41e72
John Greth <jgreth0@gmail.com>
2024-12-14 11:18:40 -0500

    Day13 perf

--- src/day13.rs
+++ src/day13.rs
@@ -1,9 +1,9 @@
 
 // https://adventofcode.com/2024/day/13
 
-use scanf::sscanf;
+// use scanf::sscanf;
 
-fn get_total(input: &str, part2: bool) -> i64 {
+unsafe fn get_total(input: &str, part2: bool) -> i64 {
     let mut x1: Vec<i64> = Vec::with_capacity(320);
     let mut y1: Vec<i64> = Vec::with_capacity(320);
     let mut x2: Vec<i64> = Vec::with_capacity(320);
@@ -13,19 +13,63 @@ fn get_total(input: &str, part2: bool) -> i64 {
 
     for line in input.lines() {
         let bytes = line.as_bytes();
-        let mut x: i64 = 0;
-        let mut y: i64 = 0;
+        // This quite dirty parsing is 10x faster than sscanf :/
         if let Some(b) = bytes.get(7) {
             if *b == b'A' {
-                sscanf!(line, "Button A: X+{}, Y+{}", x, y).unwrap();
+                // sscanf!(line, "Button A: X+{}, Y+{}", x, y).unwrap();
+                let mut x = (bytes.get_unchecked(12) - b'0') as i64;
+                let mut pos = 13;
+                let mut b = bytes.get_unchecked(pos);
+                while *b != b',' {
+                    x = 10 * x + (*b - b'0') as i64;
+                    pos += 1;
+                    b = bytes.get_unchecked(pos);
+                }
+                pos += 4;
+                let mut y = (bytes.get_unchecked(pos) - b'0') as i64;
+                pos += 1;
+                while pos < bytes.len() {
+                    y = 10 * y + (bytes.get_unchecked(pos) - b'0') as i64;
+                    pos += 1;
+                }
                 x1.push(x);
                 y1.push(y);
             } else if *b == b'B' {
-                sscanf!(line, "Button B: X+{}, Y+{}", x, y).unwrap();
+                // sscanf!(line, "Button B: X+{}, Y+{}", x, y).unwrap();
+                let mut x = (bytes.get_unchecked(12) - b'0') as i64;
+                let mut pos = 13;
+                let mut b = bytes.get_unchecked(pos);
+                while *b != b',' {
+                    x = 10 * x + (*b - b'0') as i64;
+                    pos += 1;
+                    b = bytes.get_unchecked(pos);
+                }
+                pos += 4;
+                let mut y = (bytes.get_unchecked(pos) - b'0') as i64;
+                pos += 1;
+                while pos < bytes.len() {
+                    y = 10 * y + (bytes.get_unchecked(pos) - b'0') as i64;
+                    pos += 1;
+                }
                 x2.push(x);
                 y2.push(y);
             } else if *b == b'X' {
-                sscanf!(line, "Prize: X={}, Y={}", x, y).unwrap();
+                // sscanf!(line, "Prize: X={}, Y={}", x, y).unwrap();
+                let mut x = (bytes.get_unchecked(9) - b'0') as i64;
+                let mut pos = 10;
+                let mut b = bytes.get_unchecked(pos);
+                while *b != b',' {
+                    x = 10 * x + (*b - b'0') as i64;
+                    pos += 1;
+                    b = bytes.get_unchecked(pos);
+                }
+                pos += 4;
+                let mut y = (bytes.get_unchecked(pos) - b'0') as i64;
+                pos += 1;
+                while pos < bytes.len() {
+                    y = 10 * y + (bytes.get_unchecked(pos) - b'0') as i64;
+                    pos += 1;
+                }
                 if part2 {
                     x += 10000000000000;
                     y += 10000000000000;
@@ -36,12 +80,6 @@ fn get_total(input: &str, part2: bool) -> i64 {
         }
     }
     let count: usize = x1.len();
-    let x1: &[i64] = &x1.as_slice()[0..count];
-    let y1: &[i64] = &y1.as_slice()[0..count];
-    let x2: &[i64] = &x2.as_slice()[0..count];
-    let y2: &[i64] = &y2.as_slice()[0..count];
-    let x3: &[i64] = &x3.as_slice()[0..count];
-    let y3: &[i64] = &y3.as_slice()[0..count];
     let mut total: Vec<i64> = vec![0; count];
     let total: &mut [i64] = &mut total.as_mut_slice()[0..count];
 
@@ -71,23 +109,26 @@ fn get_total(input: &str, part2: bool) -> i64 {
     // else if da == 0 or db == 0, there are no solutions.
     // else there is one possible solution. Check if a and b are ints in the correct range.
     
-    // TODO: Not sure what is preventing autovectorization here...
+    // TODO: Is this correctly autovectorized?
+    #[allow(clippy::needless_range_loop)]
     for c in 0..count {
-        let x1 = x1[c];
-        let y1 = y1[c];
-        let x2 = x2[c];
-        let y2 = y2[c];
-        let x3 = x3[c];
-        let y3 = y3[c];
+        let x1 = x1.get_unchecked(c);
+        let y1 = y1.get_unchecked(c);
+        let x2 = x2.get_unchecked(c);
+        let y2 = y2.get_unchecked(c);
+        let x3 = x3.get_unchecked(c);
+        let y3 = y3.get_unchecked(c);
         let na = (x3 * y2) - (x2 * y3);
         let da = (x1 * y2) - (x2 * y1);
         let nb = (x3 * y1) - (x1 * y3);
         let db = 0 - da; // Note the expressions above are the same but reversed.
-        assert!(da != 0);
+        debug_assert!(da != 0);
+        core::hint::assert_unchecked(da != 0);
         let a = na / da;
+        core::hint::assert_unchecked(db != 0);
         let b = nb / db;
         total[c] = if 0 <= a && 0 <= b && a * da == na && b * db == nb {
-            assert!(part2 || (a <= 100 && b <= 100));
+            debug_assert!(part2 || (a <= 100 && b <= 100));
             3 * a + b
         } else {
             0
@@ -99,12 +140,12 @@ fn get_total(input: &str, part2: bool) -> i64 {
 
 #[aoc(day13, part1)]
 pub fn part1(input: &str) -> i64 {
-    get_total(input, false)
+    unsafe {get_total(input, false)}
 }
 
 #[aoc(day13, part2)]
 pub fn part2(input: &str) -> i64 {
-    get_total(input, true)
+    unsafe {get_total(input, true)}
 }
 
 #[cfg(test)]
